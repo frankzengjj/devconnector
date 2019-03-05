b5c80c990bb3bedf89030fa84a4dc24f138070c2
frankzengjj <tianyuzeng1997@gmail.com>
2019-03-05 03:07:03 -0600

    login and token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,75 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrpt = require('bcryptjs');
+
+// Load User Model
+const User = require('../../models/User');
 
 // @route  GET api/users/test
 // @desc   Tests users route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: 'Users Works'}));
+
+// @route  GET api/users/register
+// @desc   Register user
+// @access Public
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(user) {
+                return res.status(400).json({email: 'Email Already Exists'});
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // size
+                    r: 'pg', // pg
+                    d: 'mm' // Default
+                });
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+
+                bcrpt.genSalt(10, (err, salt) =>{
+                    bcrpt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        });
+});
+
+// @route  GET api/users/login
+// @desc   Login user / Returning JWT token
+// @access Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({email})
+        .then(user => {
+            // Check for user
+            if(!user){
+                return res.status(404).json({email: 'User not found'});
+            }
+
+            // Check password
+            bcrpt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({msg:'Success'});
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'});
+                    }
+                });
+        })
+})
 
 module.exports = router;
